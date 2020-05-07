246c6af983466d4cfda1945bf2c60b15bbd72215
Toby Boudreaux <toby.boudreaux11@gmail.com>
2020-05-07 17:12:58 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -8,7 +8,7 @@ while row_index < src.count do
   element_index = 0
   smallest_element = 100
   while element_index < src[row_index].count do 
-    if src[row_index][element_index][0] < smallest_element
+    if src[row_index][element_index].count < smallest_element
       smallest_element = src[row_index][element_index]
   end 
     element_index += 1
